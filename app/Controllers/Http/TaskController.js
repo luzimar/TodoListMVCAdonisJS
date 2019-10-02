577461e10c44d7b63b6f31a5f28e06f613b8f917
luzimar <lzm.spfc@hotmail.com>
2019-10-01 23:10:11 -0300

    First commit

---
+++ app/Controllers/Http/TaskController.js
@@ -0,0 +1,61 @@
+"use strict";
+const Task = use("App/Models/Task");
+const { validateAll } = use("Validator");
+
+class TaskController {
+  async index({ view }) {
+    const taskList = await Task.all();
+    return view.render("tasks", {
+      title: "Latest tasks",
+      tasks: taskList.toJSON()
+    });
+  }
+
+  async store({ request, response, session }) {
+    const messages = {
+      "title.required": "Required",
+      "title.min": "min 3"
+    };
+
+    const validation = await validateAll(
+      request.all(),
+      {
+        title: "required|min:5|max:140",
+        description: "required|min:10"
+      },
+      messages
+    );
+
+    if (validation.fails()) {
+      session.withErrors(validation.messages()).flashAll();
+      return response.redirect("back");
+    }
+    const task = new Task();
+    task.title = request.input("title");
+    task.description = request.input("description");
+
+    await task.save();
+
+    session.flash({ notification: "Task added!" });
+
+    return response.redirect("/tasks");
+  }
+
+  async detail({ params, view }) {
+    const task = await Task.find(params.id);
+
+    return view.render("detail", {
+      task: task
+    });
+  }
+
+  async remove({ params, response, session }) {
+    const task = await Task.find(params.id);
+    await task.delete();
+    session.flash({ notification: "Task removed!" });
+
+    return response.redirect("/tasks");
+  }
+}
+
+module.exports = TaskController;
